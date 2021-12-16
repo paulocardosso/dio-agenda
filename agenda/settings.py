0053cc93497bcb7c442ceed44e1584eb81e54f48
Paulo Henrique Oliveira Cardoso <pauloshake@gmail.com>
2021-12-16 11:30:31 -0300

    Criado paginas HTML para listar eventos

--- agenda/settings.py
+++ agenda/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], #os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', #os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
