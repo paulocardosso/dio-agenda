c1330f2a0da8aa5ac5d363c145827fa013ce7f12
Paulo Henrique Oliveira Cardoso <pauloshake@gmail.com>
2021-12-17 09:16:28 -0300

    Incluido as funções de autenticacao

--- core/views.py
+++ core/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render #,redirect
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 """
 def index(request):
     return redirect('/agenda/')
 """
+def login_user(request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario e/ou senha invalidos")
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request,'agenda.html', dados)
