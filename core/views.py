2bcecaab5013ef3fdcd1fe4443eaf886d1424b04
Paulo Henrique Oliveira Cardoso <pauloshake@gmail.com>
2021-12-20 15:11:19 -0300

    Incluido um novo atributo local do evento

--- core/views.py
+++ core/views.py
@@ -42,10 +42,12 @@ def  evento(request):
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
+        local_evento = request.POST.get('local_evento')
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
         Evento.objects.create(titulo=titulo,
+                              local_evento=local_evento,
                               data_evento=data_evento,
                               descricao=descricao,
                               usuario=usuario)
